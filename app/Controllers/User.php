a33b0612c4771a10b9a8be5983e293972e7c313e
awan-ama <mrakaazwar@gmail.com>
2024-06-29 09:32:51 +0800

    user routes and controller added

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -7,4 +7,31 @@ class User extends BaseController
     {
         return view('dashboard_view');
     }
+    
+    public function eServices()
+    {
+        return view('e-services_view');
+    }
+    public function eCommandCenter()
+    {
+        return view('e-command_center_view');
+    }
+    public function eResponse(){
+        return view('e-response_view');
+    }
+
+    public function tanyaFT(){
+        return view('tanyaft1_view');
+    }
+    public function pengajuanTanyaFT(){
+        return view('Pengajuan_tanyaft_view');
+    }
+
+    public function detailTanyaFT(){
+        return view('detail_tanyaft_view');
+    }
+
+    public function tanggapanTanyaFT(){
+        return view('tanggapan_tanyaft_view');
+    }
 }
